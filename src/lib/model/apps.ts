e28c2b9bee64db617b543d98e4d9bd6f41d2b431
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2024-02-19 13:33:11 -0500

    Add learn flex to model and learn flex's privacy policy.

--- src/lib/model/apps.ts
+++ src/lib/model/apps.ts
@@ -1,6 +1,7 @@
 import clockSuiteAppIcon from '$lib/images/ClockSuiteAppIcon.png';
 import filterArtAppIcon from '$lib/images/FilterArtAppIcon.png';
 import spiffyClockAppIcon from '$lib/images/SpiffyClockAppIcon.png';
+import learnFlexAppIcon from '$lib/images/LearnFlexAppIcon.png';
 import contactCardsAppIcon from '$lib/images/ContactCardsAppIcon.png';
 
 class AppModel {
@@ -15,15 +16,23 @@ class AppModel {
   }
 }
 
+const clockSuite = new AppModel("Clock Suite", "clocksuite", clockSuiteAppIcon)
+const filterArt = new AppModel("Filter Art", "filterart", filterArtAppIcon)
+const spiffyClock = new AppModel("Spiffy Clock", "spiffyclock", spiffyClockAppIcon)
+const learnFlex = new AppModel("Learn Flex", "learnflex", learnFlexAppIcon)
+const contactCards = new AppModel("Contact Cards", "contactcards", contactCardsAppIcon)
+
 export const apps = [
-  new AppModel("Clock Suite", "clocksuite", clockSuiteAppIcon),
-  new AppModel("Filter Art", "filterart", filterArtAppIcon),
-  new AppModel("Spiffy Clock", "spiffyclock", spiffyClockAppIcon),
-  new AppModel("Contact Cards", "contactcards", contactCardsAppIcon)
+  clockSuite,
+  filterArt,
+  spiffyClock,
+  learnFlex,
+  contactCards
 ]
 
 export const currentApps = [
-  new AppModel("Clock Suite", "clocksuite", clockSuiteAppIcon),
-  new AppModel("Filter Art", "filterart", filterArtAppIcon),
-  new AppModel("Spiffy Clock", "spiffyclock", spiffyClockAppIcon),
+  clockSuite,
+  filterArt,
+  spiffyClock,
+  learnFlex,
 ]
---
+++ src/routes/privacy/learnflex/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import AppHeading from '../../components/AppHeading.svelte';
+  import learnFlexAppIcon from '$lib/images/LearnFlexAppIcon.png'
+</script>
+
+<svelte:head>
+	<title>Celeritas Apps | Learn Flex Privacy</title>
+	<meta name="description" content="Learn Flex privacy policy" />
+</svelte:head>
+
+<AppHeading imgSrc={learnFlexAppIcon} ariaLabel="Learn Flex Privacy Policy">Learn Flex Privacy Policy</AppHeading>
+<p class="body-p">Learn Flex does not collect data from its users, and does not connect to the internet. Any changes to the privacy policy of Learn Flex will be reflected here.</p>
+<br />
+<br />
+<br />
+<br />
