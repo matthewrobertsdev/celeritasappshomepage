0a6207bb4365e949edb9e38522439d9bef019844
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2024-02-24 13:31:50 -0500

    Add missing base path reference

--- src/routes/components/HeaderHomeLink.svelte
+++ src/routes/components/HeaderHomeLink.svelte
@@ -2,8 +2,9 @@
 	import { page } from '$app/stores';
 	import celeritasAppsLogo from '$lib/images/celeritas-apps.png';
   import './header.css'
+  import { base } from '$app/paths';
 </script>
 
 <li aria-current={$page.url.pathname === '/' ? 'page' : undefined} class={$page.url.pathname === '/' ? 'active-home' : 'home'}>
-  <a href="/" aria-label="Celeritas Apps"><img src={celeritasAppsLogo} class="home-img" alt="Celeritas Apps Logo"> Celeritas Apps</a>
+  <a href="{base}/" aria-label="Celeritas Apps"><img src={celeritasAppsLogo} class="home-img" alt="Celeritas Apps Logo"> Celeritas Apps</a>
 </li>
