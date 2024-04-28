5b6bbd2f2c8d64ea4352e14ba13c3d33d5959873
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2024-04-27 23:21:55 -0400

    Add start of Celebration Lights pages.

---
+++ src/routes/apps/celebrationlights/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import AppHeading from '../../components/AppHeading.svelte';
+  import celebrationLightsAppIcon from '$lib/images/CelebrationLightsAppIcon.png'
+</script>
+
+<svelte:head>
+	<title>Celeritas Apps | Celebration Lights</title>
+	<meta name="description" content="Celebration Lights app homepage" />
+</svelte:head>
+
+<AppHeading imgSrc={celebrationLightsAppIcon} ariaLabel="Celebration Lights">Celebration Lights</AppHeading>
+<h2 class="left-h2">Decorate your screen with celebratory lights!</h2>
+<p class="body-p">Whether it is a holiday or not, surround your screen with colorful lights.</p>
+<p class="body-p">Included are lights with different colors from the rainbow, lights with different holiday-themed colors, the ability to have lights turn on and off, and multiple shapes for the bulbs.</p>
+<br />
+<br />
+<br />
+<br />
---
+++ src/routes/privacy/celebrationlights/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import AppHeading from '../../components/AppHeading.svelte';
+  import celebrationLightsAppIcon from '$lib/images/CelebrationLightsAppIcon.png'
+</script>
+
+<svelte:head>
+	<title>Celeritas Apps | Celebrations Lights Privacy</title>
+	<meta name="description" content="Celebrations Lights privacy policy" />
+</svelte:head>
+
+<AppHeading imgSrc={celebrationLightsAppIcon} ariaLabel="Celbrations Lights Privacy Policy">Celbrations Lights Privacy Policy</AppHeading>
+<p class="body-p">Celebrations Lights does not collect data from its users, and does not connect to the internet. Any changes to the privacy policy of Celebrations Lights will be reflected here.</p>
+<br />
+<br />
+<br />
+<br />
