76c25ca82b7655330201145bd5bbc09c8e2a2123
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2024-04-28 14:29:59 -0400

    Fix typo in celebration lights privacy policy.

--- src/routes/privacy/celebrationlights/+page.svelte
+++ src/routes/privacy/celebrationlights/+page.svelte
@@ -8,7 +8,7 @@
 	<meta name="description" content="Celebrations Lights privacy policy" />
 </svelte:head>
 
-<AppHeading imgSrc={celebrationLightsAppIcon} ariaLabel="Celbrations Lights Privacy Policy">Celbrations Lights Privacy Policy</AppHeading>
+<AppHeading imgSrc={celebrationLightsAppIcon} ariaLabel="Celebrations Lights Privacy Policy">Celebrations Lights Privacy Policy</AppHeading>
 <p class="body-p">Celebrations Lights does not collect data from its users, and does not connect to the internet. Any changes to the privacy policy of Celebrations Lights will be reflected here.</p>
 <br />
 <br />
