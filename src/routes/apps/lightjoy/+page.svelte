dabb1eae1d3682379ff7bf043500a46332d3987e
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2024-12-07 13:39:39 -0500

    Update light joy page

--- src/routes/apps/lightjoy/+page.svelte
+++ src/routes/apps/lightjoy/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import AppHeading from '../../components/AppHeading.svelte';
-  import celebrationLightsAppIcon from '$lib/images/CelebrationLightsAppIcon.png'
-  import celebrationLights from '$lib/images/LightJoy.png'
+	import celebrationLightsAppIcon from '$lib/images/CelebrationLightsAppIcon.png';
+	import celebrationLights from '$lib/images/LightJoy.png';
 </script>
 
 <svelte:head>
@@ -10,108 +10,118 @@
 </svelte:head>
 
 <AppHeading imgSrc={celebrationLightsAppIcon} ariaLabel="Light Joy">Light Joy</AppHeading>
-<h2 class="left-h2">Decorate your screen with lights all year round or for holidays or special events!</h2>
-<p class="body-p">Decorate your screen with colorful lights, both all year round and with special themes for holidays and special events!  Single color, custom color, multi-color, and themed colors are available, as well as various bulb shapes, on/off modes for the lights, various cord colors, float-on-top mode or not, and ways of showing and hiding different strands of lights.  Lights will appear on all your displays for your computer.</p>
-<img src={celebrationLights} class="mac-image" alt="Rainbow colored lights around the edge of a computer screen.">
+<h2 class="left-h2">
+	Decorate your screen with lights all year round or for holidays or special occasions!
+</h2>
+<p class="body-p">
+	Decorate your screen with colorful lights, both all year round and with special themes for
+	holidays and special occasions! Single color, custom color, changing, and holiday and
+	special-occasion themes are available, as well as various bulb shapes, on/off modes for the
+	lights, various cord colors, float-on-top mode or not, and ways of showing and hiding different
+	strands of lights. Lights will appear on all your displays for your computer. You can use your
+	computer as usual with the lights around the edges as a decoration. In-app purchase is required to
+	use all themes besides rainbow.
+</p>
+<p class="body-p">Note: does not go over other apps that are in fullscreen mode.</p>
+<img
+	src={celebrationLights}
+	class="mac-image"
+	alt="Rainbow colored lights around the edge of a computer screen."
+/>
 <h2 class="left-h2">Single colors:</h2>
-  <ul class="feature-list">
-  <li>—Red</li>
-  <li>—Orange</li>
-  <li>—Yellow</li>
-  <li>—Green</li>
-  <li>—Mint</li>
-  <li>—Cyan</li>
-  <li>—Teal</li>
-  <li>—Blue</li>
-  <li>—Indigo</li>
-  <li>—Purple</li>
-  <li>—Pink</li>
-  <li>—Brown</li>
-  <li>—White</li>
-  <li>—Black</li>
-</ul>
-
-<h2 class="left-h2">Multi-Color Options:</h2>
 <ul class="feature-list">
-<li>—Rainbow</li>
-<li>—Red, blue, yellow, green and orange</li>
-<li>—Changing colors every second according to the rainbow</li>
+	<li>—Red</li>
+	<li>—Orange</li>
+	<li>—Yellow</li>
+	<li>—Green</li>
+	<li>—Mint</li>
+	<li>—Cyan</li>
+	<li>—Teal</li>
+	<li>—Blue</li>
+	<li>—Indigo</li>
+	<li>—Purple</li>
+	<li>—Pink</li>
+	<li>—Brown</li>
+	<li>—White</li>
+	<li>—Black</li>
+  <li>—Custom Color</li>
+  <li>—Changing colors every second according to the rainbow</li>
 </ul>
 
 <h2 class="left-h2">Themed Lights:</h2>
 <ul class="feature-list">
-<li>—Christmas</li>
-<li>—Christmas Mix</li>
-<li>—Ornaments Mix</li>
-<li>—Ice and Snow</li>
-<li>—New Year's</li>
-<li>—Milestone</li>
-<li>—Autumn</li>
-<li>—Halloween</li>
-<li>—Thanksgiving</li>
-<li>—Fourth of July</li>
-<li>—Hanukkah</li>
-<li>—Earth Day</li>
-<li>—Valentine's Day</li>
-<li>—Kwanzaa</li>
-<li>—Ramadan</li>
-<li>—Diwali</li>
-<li>—Buddha Day</li>
-<li>—Cinco de Mayo</li>
-<li>—Saint Patrick's Day</li>
-<li>—Lunar New Year</li>
-<li>—Easter</li>
-<li>—Birthday</li>
+  <li>—Rainbow</li>
+  <li>—Multi-color: red, blue, yellow, green and orange</li>
+	<li>—Christmas</li>
+	<li>—Christmas Mix</li>
+	<li>—Ornaments Mix</li>
+	<li>—Ice and Snow</li>
+	<li>—New Year's</li>
+	<li>—Milestone</li>
+	<li>—Autumn</li>
+	<li>—Halloween</li>
+	<li>—Thanksgiving</li>
+	<li>—Fourth of July</li>
+	<li>—Hanukkah</li>
+	<li>—Earth Day</li>
+	<li>—Valentine's Day</li>
+	<li>—Kwanzaa</li>
+	<li>—Ramadan</li>
+	<li>—Diwali</li>
+	<li>—Buddha Day</li>
+	<li>—Cinco de Mayo</li>
+	<li>—Saint Patrick's Day</li>
+	<li>—Lunar New Year</li>
+	<li>—Easter</li>
+	<li>—Birthday</li>
 </ul>
 
 <h2 class="left-h2">Bulb Shapes:</h2>
 <ul class="feature-list">
-<li>—Basic</li>
-<li>—Curved</li>
-<li>—Circular</li>
-<li>—Hearts</li>
-<li>—Stars</li>
-<li>—Moons</li>
-<li>—Moons and Stars</li>
-<li>—Ice and Snow</li>
-<li>—Bells</li>
-<li>—Holiday Variety</li>
-<li>—Balloons</li>
-<li>—Bursts</li>
+	<li>—Basic</li>
+	<li>—Curved</li>
+	<li>—Circular</li>
+	<li>—Hearts</li>
+	<li>—Stars</li>
+	<li>—Moons</li>
+	<li>—Moons and Stars</li>
+	<li>—Bells</li>
+	<li>—Balloons</li>
+	<li>—Bursts</li>
 </ul>
 
 <h2 class="left-h2">Cord Colors:</h2>
 <ul class="feature-list">
-  <li>—Red</li>
-  <li>—Orange</li>
-  <li>—Yellow</li>
-  <li>—Green</li>
-  <li>—Blue</li>
-  <li>—Indigo</li>
-  <li>—Purple</li>
-  <li>—Pink</li>
-  <li>—Brown</li>
-  <li>—White</li>
-  <li>—Black</li>
+	<li>—Red</li>
+	<li>—Orange</li>
+	<li>—Yellow</li>
+	<li>—Green</li>
+	<li>—Blue</li>
+	<li>—Indigo</li>
+	<li>—Purple</li>
+	<li>—Pink</li>
+	<li>—Brown</li>
+	<li>—White</li>
+	<li>—Black</li>
 </ul>
 
 <h2 class="left-h2">On/Off Modes:</h2>
 <ul class="feature-list">
-<li>—All On</li>
-<li>—Random Off</li>
-<li>—Staggered Off</li>
-<li>—All Off</li>
+	<li>—All On</li>
+	<li>—Random Off</li>
+	<li>—Staggered Off</li>
+	<li>—All Off</li>
 </ul>
 
 <h2 class="left-h2">Strand Visibility:</h2>
 <ul class="feature-list">
-<li>—All Visible</li>
-<li>—Top Hidden</li>
-<li>—Bottom Hidden</li>
-<li>—Top and Bottom Hidden</li>
-<li>—Sides Hidden</li>
-<li>—Only Top Visible</li>
-<li>—Only Bottom Visible</li>
+	<li>—All Visible</li>
+	<li>—Top Hidden</li>
+	<li>—Bottom Hidden</li>
+	<li>—Top and Bottom Hidden</li>
+	<li>—Sides Hidden</li>
+	<li>—Only Top Visible</li>
+	<li>—Only Bottom Visible</li>
 </ul>
 
 <br />
