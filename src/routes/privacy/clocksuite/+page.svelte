9933f7ede1aa90abb0757a94eb8ed73146f38151
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2025-06-14 15:30:37 -0400

    Update privacy policies for Light Joy and Clock Suite

--- src/routes/privacy/clocksuite/+page.svelte
+++ src/routes/privacy/clocksuite/+page.svelte
@@ -9,7 +9,7 @@
 </svelte:head>
 
 <AppHeading imgSrc={clockSuiteAppIcon} ariaLabel="Clock Suite Privacy Policy">Clock Suite Privacy Policy</AppHeading>
-<p class="body-p">Clock Suite does not collect data from its users, and does not connect to the internet. Any changes to the privacy policy of Clock Suite will be reflected here.</p>
+<p class="body-p">Clock Suite does not collect data from its users.  It only connects to the internet when you make an in-app purchase or when it needs to verify an in-app purchase.  Any changes to the privacy policy of Clock Suite will be reflected here.</p>
 <br />
 <br />
 <br />
