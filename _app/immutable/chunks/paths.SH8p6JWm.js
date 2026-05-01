3e169d49d4167f9faccaf5e095e3f8f4008e6ee2
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2026-04-30 22:08:30 -0400

    Updates

---
+++ _app/immutable/chunks/paths.SH8p6JWm.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1wt7gt9)==null?void 0:s.base)??"/celeritasappshomepage";var t;const a=((t=globalThis.__sveltekit_1wt7gt9)==null?void 0:t.assets)??e;export{a,e as b};
